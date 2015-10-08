b7caf961171e9a694cc0afbc9d24824306092084
Andreas Lind <andreas@one.com>
2015-10-08 23:00:59 +0200

    generate-site.js: Use a fixed width for the output.

--- generate-site.js
+++ generate-site.js
@@ -4,6 +4,8 @@ var argv = require('minimist')(process.argv.slice(2));
 var unexpected = require('unexpected').clone()
     .installPlugin(require('./lib/unexpectedExif'));
 
+unexpected.output.preferredWidth = 80;
+
 global.require = require;
 
 var generator = require('unexpected-documentation-site-generator');
